8f8b9ee0061770fd20e5ce37cba67cf191f7524d
ClauZ <clauz9@users.noreply.github.com>
2024-02-27 13:50:20 +0200

    Build only universal APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -244,7 +244,7 @@ android {
       abi {
         isEnable = !project.hasProperty("generateBaselineProfile")
         reset()
-        include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+//        include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
         isUniversalApk = true
       }
     }
